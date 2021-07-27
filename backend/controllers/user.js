ef8e118bd63e4288c0fab5c3faec300721e1b12b
Nathan Akpawu <nakpawu@gmail.com>
2021-07-27 16:10:44 +0000

    Crypt password and mask  email

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,22 +1,21 @@
-const models = require('../models');
+const bcrypt = require('bcrypt');
+const maskdata = require('maskdata');
+
+const User = require('../models').User;
 
 exports.signup = (req, res, next) => {
-    const user = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password 
-    };
-    models.User.create(user).then(result => {
-        res.status(201).json({
-            message: 'Utilisateur crée avec succès !',
-            post: result
-        });
-    }).catch(error => {
-        res.status(500).json({
-            message: 'Impossible de créer cet utilisateur',
-            post: error
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = {
+                name: req.body.name,
+                email: maskdata.maskEmail2(req.body.email),
+                password: hash
+            };
+            User.create(user)
+                .then(() => res.status(201).json({ message: 'Utilisateur créé avec succès !' }))
+                .catch(error => res.status(400).json({ message: 'Impossible de créer cet utilisateur', error }));
         })
-    })
+        .catch(error => res.status(500).json({ error }))
 };
 
 exports.login = (req, res) => {
