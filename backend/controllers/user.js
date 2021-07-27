fafe964931f4dc741e886e153fa84a20e9c140d6
Nathan Akpawu <nakpawu@gmail.com>
2021-07-27 12:37:08 +0000

    Insert into user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,22 @@
-exports.signup = (req, res) => {
-    res.json({ message: 'Pour l\'inscription !!!' });
+const models = require('../models');
+
+exports.signup = (req, res, next) => {
+    const user = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password 
+    };
+    models.User.create(user).then(result => {
+        res.status(201).json({
+            message: 'Utilisateur crée avec succès !',
+            post: result
+        });
+    }).catch(error => {
+        res.status(500).json({
+            message: 'Impossible de créer cet utilisateur',
+            post: error
+        })
+    })
 };
 
 exports.login = (req, res) => {
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const userController = require('../controllers/user');
 
-router.use('/signup', userController.signup);
+router.post('/signup', userController.signup);
 router.use('/login', userController.login);
 
 module.exports = router;
